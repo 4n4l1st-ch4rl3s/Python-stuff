029fcc144141590dc99d9c2bb20380f90f8f9748
4n4l1st-ch4rl3s <63286909+4n4l1st-ch4rl3s@users.noreply.github.com>
2023-01-16 10:55:28 +0300

    added comments

--- process xml files/xml_parsing_1.py
+++ process xml files/xml_parsing_1.py
@@ -8,17 +8,23 @@ root   = tree.getroot()
 
 # root = ET.fromstring(your_xml_as_string)
 
+#Show the root element
 print('The root tag is: ', root.tag)
+#Show the number of elements in the root
 print('The number of children is: ', len(root))
 print('The root has the following children: ')
+#Ireterates between the children of the root and shows their contents
 for child in root:
+    #prints out the child and its attributes
     print(child.tag, child.attrib)
 
 print('\n')
 print('\n')
 
 print('My books: \n')
+#iterates over the elements of the root and and stores it the the variable books
 for book in root:
+    print(book.attrib, '\n')# dispalys the attributes
     print('Title: ', book.attrib['title'])
     print('Author: ', book[0].text)
     print('Year: ', book[1].text, '\n')
